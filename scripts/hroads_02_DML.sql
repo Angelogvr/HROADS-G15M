cdaf9756ed0565660aac0d3087b3b85ea5208341
Daniele Vieira <angelogabrielvr@gmail.comgit remote deletegit reset --hard HEAD~1git status>
2021-08-10 23:19:12 -0300

    Trabalho concluído

--- scripts/hroads_02_DML.sql
+++ scripts/hroads_02_DML.sql
@@ -7,16 +7,21 @@ GO
 
 SELECT * FROM TipoHabilidade
 
-INSERT INTO Habilidades(nomeHabilidade)
-VALUES ('Lança mortal'),('Escudo Supremo'),('Recuperar Vida');
+INSERT INTO Habilidades(nomeHabilidade, idTipoHabilidade)
+VALUES ('Lança mortal', '1'),('Escudo Supremo', '2'),('Recuperar Vida','3');
 GO
 
 SELECT * FROM Habilidades
 
 
-INSERT INTO HabClasses
-VALUES (1
+INSERT INTO HabClasses (idHabilidade, idClasses)
+VALUES (7,1),(8,1),
+		(8,2),(7,3),
+		(8,4),(9,4),
+		(9,6)
 GO
+
+--A ID da habilidade acima está como 7/8/9 por conta de uma complicação que tive que não fui capaz de alterar (angelo)
 
 INSERT INTO Classes (nomeClasse)
 VALUES  ('Bárbaro'),('Cruzado'),('Caçadora de Demônios'),('Monge'),('Necromante'),('Feiticeiro'),('Arcanista');
@@ -35,4 +40,8 @@ UPDATE Personagem
 SET nomePersonagem = 'Fer7'
 WHERE idPersonagem = 3
 
+UPDATE Classes
 
+SET nomeClasse = 'Necromancer'
+WHERE idClasses = 5;
+GO
